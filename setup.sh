47bfbd1c275738355776070ee658ce2c6a5c9ae6
Kenneth Rodriguez <kennethrdz@hotmail.com>
2023-01-19 23:31:23 -0600

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+#Basic tools
+sudo apt install git vim trash-cli libreoffice
+
+#Printer drivers
+sudo apt install printer-driver-escpr
+
+#Development tools
+sudo apt install build-essential openjdk-11-jdk libgtk-4-dev postgresql
+
+#Useful tools
+sudo apt install gparted pdfarranger ffmpeg vlc gnucash cheese obs-studio okular
+
+#Funny stuff
+sudo apt install cmatrix sl
+
+#Games
+sudo apt install gnuchess gnugo xboard
